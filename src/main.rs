443e11194bcb3d8bdd97d5da45c62d4a337b22ad
Daniel Etuk <danny.hex12@gmail.com>
2024-01-27 18:08:03 +0100

    Improve telemetry instrumentation

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,20 @@ use deezpatch::{
     startup::run,
     telemetry::{get_subscriber, init_subscriber},
 };
+use secrecy::ExposeSecret;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let subscriber = get_subscriber("deezpatch".into(), "info".into());
+    let subscriber = get_subscriber("deezpatch".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to Postgres");
+    let connection_pool =
+        PgPool::connect(&configuration.database.connection_string().expose_secret())
+            .await
+            .expect("Failed to connect to Postgres");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address).expect("Failed to bind random port");
     run(listener, connection_pool)?.await
