5e1562d4d4e7d1ad6731f8b0edbf7d0bfa507d7b
Daniel Etuk <danny.hex12@gmail.com>
2024-01-27 01:05:02 +0100

    Add basic telemetry

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
-use deezpatch::{configuration::get_configuration, startup::run};
+use deezpatch::{
+    configuration::get_configuration,
+    startup::run,
+    telemetry::{get_subscriber, init_subscriber},
+};
 use sqlx::PgPool;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    let subscriber = get_subscriber("deezpatch".into(), "info".into());
+    init_subscriber(subscriber);
+
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
